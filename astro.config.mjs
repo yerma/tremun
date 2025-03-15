01ead6dcc6daa4873434458e9686529b2845ba2b
Yermain Araya <yerma.araya@gmail.com>
2025-03-14 21:53:41 -0300

    Add base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yerma.github.io',
+	base: '/tremun',
 	integrations: [mdx(), sitemap()],
 });
